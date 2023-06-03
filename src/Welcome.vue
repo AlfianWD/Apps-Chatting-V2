5e62f5892eb0d55b109a8b066c760735ecebda36
AlfianWD <carzyman01@gmail.com>
2023-06-03 16:16:05 +0700

    First Commit

---
+++ src/Welcome.vue
@@ -0,0 +1,33 @@
+<!-- Created By Alfian Wahyu -->
+
+<template>
+  <div class="container">
+    <img
+      :class="['fade-in', { delayed: delayed }]"
+      src="./assets/img/logo-chat.png"
+      style="width: 250px"
+    />
+    <div class="container-x">
+      <h1 :class="['fade-in', { delayed: delayed }]">Welcome in Chat Apps V2</h1>
+    </div>
+    <div class="container-button">
+      <button :class="['fade-in', 'btn btn-outline-primary', { delayed: delayed }]">Mulai</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+
+export default {
+  data() {
+    return {
+      delayed: false
+    }
+  },
+
+  onMounted() {
+    this.delayed = true
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './assets/css/style.css'
+
+import { createApp } from 'vue'
+import App from './Welcome.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
+
+const app = createApp(App)
+
+app.mount('#app')
